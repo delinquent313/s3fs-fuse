1c7c2d4b763180233912dc5037cf1ff9e789f0c0
delinquent313 <delinquent313@gmail.com>
2021-04-22 01:30:04 -0400

    ignoreing salted string when decrypting after using RC4 with correct key

--- PKGBUILD
+++ PKGBUILD
@@ -1,7 +1,7 @@
 # Maintainer:  fernando villarreal <fernando.villarreal.sd@gmail.co>
 
 pkgname=s3fs-fuse-rc4
-pkgver=1.89.r1834.1eb18ec
+pkgver=1.89.r1835.3af20e9
 pkgrel=1
 pkgdesc='FUSE-based file system backed by Amazon S3 with OpenSSL support, with rc4 encryption'
 arch=('i686' 'x86_64')
--- src/fdcache_entity.cpp
+++ src/fdcache_entity.cpp
@@ -249,9 +249,11 @@ void rc4(int fd, int enc) //enc =1 for encrypting enc=0 for decrypting enc=2 for
             if (headerStat == 1)
             {
                 //Salt header detected
-                lseek(fd, SALT_LEN, SEEK_SET); //move ptr after "Salted__" 
-                printf("Salted header detected"); //pos 8
-                offset = SALT_LEN;
+                removeSalt(salt);
+                RC4(key,SALT_LEN,(const unsigned char*)salt,outBuffer);//need to decrypt salt in order to get correct output 
+                lseek(fd, SALTED_LEN_STR, SEEK_SET); //move ptr after "Salted__" /ignoring salted string in fd
+                printf("Salted header detected\n"); //pos 16
+                offset = SALTED_LEN_STR;
             }
             else if (headerStat == 0)
             {
